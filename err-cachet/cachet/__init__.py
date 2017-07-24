d646da113a800c2db9edcb2ddef0293fd4b72281
Denis 'jawa' Pompilio <denis.pompilio@nbs-system.com>
2017-07-24 17:46:38 +0200

    Adding debug log

--- err-cachet/cachet/__init__.py
+++ err-cachet/cachet/__init__.py
@@ -116,6 +116,7 @@ class Cachet(BotPlugin):
     @botcmd(split_args_with=None)
     def cachet_plugin_version(self, msg, args):
         """Show cachet plugin version"""
+        self.log.debug('Call cachet_plugin_version')
         return "Cachet plugin version: %s" % VERSION
 
     #
@@ -125,6 +126,7 @@ class Cachet(BotPlugin):
     @botcmd(split_args_with=None)
     def cachet_comp_status(self, msg, args):
         """Show components problems"""
+        self.log.debug('Call cachet_comp_status')
         worst_status = 1
         reply = ["Components problems:"]
         for component in self._components.get():
@@ -143,6 +145,7 @@ class Cachet(BotPlugin):
     @botcmd(split_args_with=None)
     def cachet_comp_list_all(self, msg, args):
         """List available components"""
+        self.log.debug('Call cachet_comp_list_all')
         reply = ["Available components:"]
         for component in self._components.get():
             c_status = int(component['status'])
@@ -154,6 +157,7 @@ class Cachet(BotPlugin):
     @botcmd(split_args_with=None)
     def cachet_comp_list_groups(self, msg, args):
         """List available component groups"""
+        self.log.debug('Call cachet_comp_list_groups')
         groups = self._components.groups.get()
         reply = ["Available groups:"]
         for group in groups:
@@ -163,6 +167,7 @@ class Cachet(BotPlugin):
     @arg_botcmd('gid', type=int, help="ID of the group")
     def cachet_comp_list_group(self, msg, gid):
         """List group's components"""
+        self.log.debug('Call cachet_comp_list_group')
         worst_status = 1
         group = list(self._components.groups.get(gid))[0]
         reply = [
@@ -182,6 +187,7 @@ class Cachet(BotPlugin):
     @arg_botcmd('cid', type=int, help="ID of the component")
     def cachet_comp_show(self, msg, cid):
         """Show component details"""
+        self.log.debug('Call cachet_comp_show')
         try:
             component = list(self._components.get(cid))[0]
         except requests.exceptions.HTTPError:
@@ -213,12 +219,14 @@ class Cachet(BotPlugin):
     @arg_botcmd('text', type=str, help="Search text")
     def cachet_comp_search(self, msg, text):
         """Search component by name"""
+        self.log.debug('Call cachet_comp_search')
         return "Strong the unimplementation is with this one"
 
     @arg_botcmd('cstatus', type=str, help="Status of the component")
     @arg_botcmd('cid', type=int, help="ID of the component")
     def cachet_comp_set_status(self, msg, cid, cstatus):
         """Set component status"""
+        self.log.debug('Call cachet_comp_set_status')
         status_info = utils.get_cstatus(cstatus)
         if not status_info:
             return "Unknown component status: %s" % (cstatus,)
@@ -233,6 +241,7 @@ class Cachet(BotPlugin):
     @arg_botcmd('--num', type=int, help="Number of entries", default=5)
     def cachet_maint_forecast(self, msg, num):
         """List upcoming maintenances"""
+        self.log.debug('Call cachet_maint_forecast')
         # Selecting scheduled operation from "incidents" and sorting
         incidents = self._incidents.get()
         scheduled = [ent for ent in incidents if ent['status'] == '0']
@@ -255,6 +264,7 @@ class Cachet(BotPlugin):
     @arg_botcmd('--num', type=int, help="Number of entries", default=5)
     def cachet_maint_last(self, msg, num):
         """List past maintenances"""
+        self.log.debug('Call cachet_maint_last')
         # Selecting scheduled operation from "incidents" and sorting
         incidents = self._incidents.get()
         scheduled = [ent for ent in incidents if ent['status'] == '0']
@@ -281,6 +291,7 @@ class Cachet(BotPlugin):
     @arg_botcmd('schedule', type=str, help="Maintenance schedule date")
     def cachet_maint_new(self, msg, schedule, cid, name, desc):
         """Creation new maintenance"""
+        self.log.debug('Call cachet_maint_new')
         return "Strong the unimplementation is with this one"
 
     #
@@ -290,6 +301,7 @@ class Cachet(BotPlugin):
     @arg_botcmd('--num', type=int, help="Number of entries", default=5)
     def cachet_inci_last(self, msg, num):
         """List last incidents"""
+        self.log.debug('Call cachet_inci_last')
         # It is actually not possible to sort incident by date via Cachet
         # We get long list of incident and sort it by ourselves
         incidents = self._incidents.get()
@@ -307,6 +319,7 @@ class Cachet(BotPlugin):
     @arg_botcmd('i_id', type=int, help="ID of the Incident")
     def cachet_inci_show(self, msg, i_id):
         """Show incident details"""
+        self.log.debug('Call cachet_inci_show')
         try:
             incident = list(self._incidents.get(i_id))[0]
         except requests.exceptions.HTTPError:
@@ -346,6 +359,7 @@ class Cachet(BotPlugin):
     @arg_botcmd('istatus', type=str, help="Status of the incident")
     def cachet_inci_new(self, msg, istatus, cstatus, cid, name, desc):
         """Declare new incident"""
+        self.log.debug('Call cachet_inci_new')
         istatus_info = utils.get_istatus(istatus)
         if not istatus_info:
             return "Unknown incident status: %s" % istatus
@@ -393,6 +407,7 @@ class Cachet(BotPlugin):
     @arg_botcmd('iid', type=int, help="ID of the Incident")
     def cachet_inci_update(self, msg, iid, istatus, cstatus, imsg):
         """Update incident"""
+        self.log.debug('Call cachet_inci_update')
         try:
             incident = list(self._incidents.get(iid))[0]
         except requests.exceptions.HTTPError:
@@ -437,12 +452,14 @@ class Cachet(BotPlugin):
     @arg_botcmd('text', type=str, help="Search text")
     def cachet_inci_search(self, msg, text):
         """Search incident by name"""
+        self.log.debug('Call cachet_inci_search')
         return "This command is still not implemented."
 
     @arg_botcmd('cid', type=int, help="ID of the component")
     @arg_botcmd('iid', type=int, help="ID of the incident")
     def cachet_inci_set_component(self, msg, iid, cid):
         """Set impacted component"""
+        self.log.debug('Call cachet_inci_set_component')
         try:
             list(self._incidents.get(iid))
         except requests.exceptions.HTTPError:
@@ -463,12 +480,14 @@ class Cachet(BotPlugin):
     @arg_botcmd('iid', type=int, help="ID of the incident")
     def cachet_inci_set_date(self, msg, iid, date):
         """Set incident's date"""
+        self.log.debug('Call cachet_inci_set_date')
         return "Cachet 2.3.10 API does not support this operation"
 
     @arg_botcmd('name', type=str, help="Incident new name")
     @arg_botcmd('iid', type=int, help="ID of the incident")
     def cachet_inci_rename(self, msg, iid, name):
         """Rename incident"""
+        self.log.debug('Call cachet_inci_rename')
         try:
             list(self._incidents.get(iid))
         except requests.exceptions.HTTPError:
@@ -479,6 +498,7 @@ class Cachet(BotPlugin):
     @arg_botcmd('iid', type=int, help="ID of the incident")
     def cachet_inci_set_hidden(self, msg, iid):
         """Hide incident from status page"""
+        self.log.debug('Call cachet_inci_set_hidden')
         try:
             list(self._incidents.get(iid))
         except requests.exceptions.HTTPError:
@@ -489,6 +509,7 @@ class Cachet(BotPlugin):
     @arg_botcmd('iid', type=int, help="ID of the incident")
     def cachet_inci_set_visible(self, msg, iid):
         """Set incident visible on status page"""
+        self.log.debug('Call cachet_inci_set_visible')
         try:
             list(self._incidents.get(iid))
         except requests.exceptions.HTTPError:
